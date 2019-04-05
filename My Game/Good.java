9443ea2de96d9b71c9f1d11f173be1cd1f207a3a
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-04-04 20:36:01 -0400

    Got score tracking working”
    d
    
    ./acp Got

---
+++ My Game/Good.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Good here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Good extends Actor
+{
+    /**
+     * Act - do whatever the Good wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkKeyPress();
+    }    
+    private void checkKeyPress()
+    {
+        if (Greenfoot.isKeyDown("right")) 
+        {
+            setLocation(getX()+4, getY());
+        }
+        
+        if (Greenfoot.isKeyDown("left")) 
+        {
+            setLocation(getX()-4, getY());
+        }
+    }
+    
+}
