96c8759c0e7fd94f1fd1e4df1c47a54865a6ce0e
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-04-04 21:07:35 -0400

    Got score tracking working”
    d
    
    ./acp stickman

--- My Game/Evil.java
+++ My Game/Evil.java
@@ -14,6 +14,6 @@ public class Evil extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        
     }    
 }
--- My Game/Good.java
+++ My Game/Good.java
@@ -8,14 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Good extends Actor
 {
-    /**
-     * Act - do whatever the Good wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private final int gravity = 1;
+    private int velocity;
     public void act() 
     {
         checkKeyPress();
+        fall();
     }    
+    public Good(){
+        velocity = 0;
+    }
     private void checkKeyPress()
     {
         if (Greenfoot.isKeyDown("right")) 
@@ -27,7 +29,21 @@ public class Good extends Actor
         {
             setLocation(getX()-4, getY());
         }
+        if (Greenfoot.isKeyDown("space") && getY() > getWorld().getHeight() - 180){
+            jump();
+        }
         
     }
+    public void fall(){
+        setLocation(getX(),getY() + velocity);
+        if(getY() > getWorld().getHeight() - 180) velocity = 0;
+        else velocity += gravity; 
+        
+    }
+    public void jump(){
+        velocity = -15;
+    }
     
+    
+
 }
